41d80feb66471a59c1d1f7ef60016647fd68b4e2
Isha Bam <79683608+Ishabam@users.noreply.github.com>
2023-03-02 10:37:24 +0530

    navbar added

---
+++ frontend/src/component/Main/HomePageProfile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './HomePageProfile.css'
+
+const HomePageProfile = () => {
+  return (
+    <div className='HomePageProfile'>
+        {/* <img src="Images/girl.jpg" alt="" /> */}
+        <p>Isha Bam</p>
+        <p>gikjncokt</p>
+    </div>
+  )
+}
+
+export default HomePageProfile
---
+++ frontend/src/component/Main/PostDisplay.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import "./PostDisplay.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHeart,
+  faHeartbeat,
+  faHeartCircleBolt,
+  faHeartCircleCheck,
+  faHeartCrack,
+  faMessage,
+} from "@fortawesome/free-solid-svg-icons";
+
+const PostDisplay = () => {
+  return (
+    <div>
+      <div className="post-display">
+        <div className="post-display-head">
+          <div className="post-display-profile">
+            <img src="Images/girl.jpg" alt="" />
+          </div>
+          <div className="post-display-heading">
+            <h3>Isha Bam</h3>
+            <p>Intern</p>
+          </div>
+        </div>
+        <div className="post-display-center">
+          <div className="post-display-content">
+            Cyber attacks are on the rise in India, and business leaders need to
+            take immediate action to combat malicious threats, reports The
+            Economic Times, citing IBM X-Force Threat Intelligence Index.
+          </div>
+          <div className="post-display-image">
+            <img src="Images/post-image.jpeg" alt="" />
+          </div>
+        </div>
+        <div className="post-display-bottom">
+          <img src="Images/heart.svg" alt="" />
+          <img src="Images/message.svg" alt="" />
+
+          {/* <FontAwesomeIcon className="fa-xl" icon={faHeart} />
+        <FontAwesomeIcon className="fa-xl" icon={faMessage} /> */}
+        </div>
+      </div>
+
+
+      <div className="post-display">
+        <div className="post-display-head">
+          <div className="post-display-profile">
+            <img src="Images/girl.jpg" alt="" />
+          </div>
+          <div className="post-display-heading">
+            <h3>Isha Bam</h3>
+            <p>Intern</p>
+          </div>
+        </div>
+        <div className="post-display-center">
+          <div className="post-display-content">
+            Cyber attacks are on the rise in India, and business leaders need to
+            take immediate action to combat malicious threats, reports The
+            Economic Times, citing IBM X-Force Threat Intelligence Index.
+          </div>
+          {/* <div className="post-display-image">
+            <img src="Images/post-image.jpeg" alt="" />
+          </div> */}
+        </div>
+        <div className="post-display-bottom">
+          <img src="Images/heart.svg" alt="" />
+          <img src="Images/message.svg" alt="" />
+
+          {/* <FontAwesomeIcon className="fa-xl" icon={faHeart} />
+        <FontAwesomeIcon className="fa-xl" icon={faMessage} /> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostDisplay;
