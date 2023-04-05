c05321233dbfd37ecbf8f9fafd5e6b60b3324e31
Anushka Shah <anushkashah@192.168.1.9>
2023-04-05 09:30:20 +0530

    added loaders

--- frontend/src/component/Main/HomePageProfile.jsx
+++ frontend/src/component/Main/HomePageProfile.jsx
@@ -2,7 +2,7 @@ import {
   faArrowUpRightFromSquare,
   faHandSparkles,
   faUserGroup,
-  faWandSparkles
+  faWandSparkles,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState, useEffect } from "react";
@@ -46,13 +46,13 @@ const HomePageProfile = () => {
   const [data, setData] = useState();
   const [college, setCollege] = useState("");
   const [allClgs, setAllClgs] = useState([]);
-  const [loading,setLoading]=useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getUser();
     getColleges();
-    setLoading(false);
-  }, [loading]);
+    // setLoading(false);
+  });
 
   const getColleges = async () => {
     const data = await fetch(`http://localhost:8000/colleges/get`);
@@ -60,10 +60,10 @@ const HomePageProfile = () => {
     console.log(res);
     let val = [];
     res.map((data) => {
-      val.push(data.name)
-    })
+      val.push(data.name);
+    });
     setAllClgs(val);
-  }
+  };
 
   const getUser = async () => {
     let result = await fetch(`http://localhost:8000/user`, {
@@ -76,7 +76,7 @@ const HomePageProfile = () => {
   };
 
   const goToProfile = () => {
-    window.location.href = '/profile';
+    window.location.href = "/profile";
   };
 
   const onAddCollege = (e) => {
@@ -84,12 +84,13 @@ const HomePageProfile = () => {
   };
 
   const handleAddSubmit = async (e) => {
+    setLoading(true);
     e.preventDefault();
     console.log(college);
     if (college) {
       let val = {
-        name: college
-      }
+        name: college,
+      };
       let data = await fetch(`http://localhost:8000/college/add`, {
         method: "POST",
         body: JSON.stringify(val),
@@ -101,9 +102,9 @@ const HomePageProfile = () => {
       const res = await data.json();
       setCollege("");
       alert(res);
-      setLoading(true);
     }
-  }
+    setLoading(false);
+  };
 
   return (
     <div className="HomePageProfile pb-3">
@@ -123,7 +124,13 @@ const HomePageProfile = () => {
         </div>
         <div className="home-profile-name-section">
           <p className="home-profile-name-section-name">{data && data.name}</p>
-          <p className="home-profile-name-section-desig" >{data && data.role == 'Super_Admin'  ? 'Super Admin': data && data.role == 'Club_Member' ? 'Club Member' : data && data.role}</p>
+          <p className="home-profile-name-section-desig">
+            {data && data.role == "Super_Admin"
+              ? "Super Admin"
+              : data && data.role == "Club_Member"
+              ? "Club Member"
+              : data && data.role}
+          </p>
         </div>
       </div>
 
@@ -139,11 +146,25 @@ const HomePageProfile = () => {
               onChange={onAddCollege}
             />
             <button
-              className=" p-1 rounded w-[60px] ml-2 bg-green-600 text-white font-[600] text-[1rem] hover:bg-green-800 transition-all ease-linear duration-2000 "
-              type="submit"
+            className=" p-1 rounded w-[60px] ml-2 bg-green-600 text-white font-[600] text-[1rem] hover:bg-green-800 transition-all ease-linear duration-2000 "
+            type="submit"
             >
-              Add
+            {loading ? (
+              <div
+                class="spinner-border text-white"
+                role="status"
+                style={{ height: "15px", width: "15px",marginLeft:"2px"}}
+              >
+                <span class="visually-hidden">Loading...</span>
+              </div>
+              
+            ) : (
+              <div>
+                Add
+              </div>
+            )}
             </button>
+            
           </form>
         </div>
 
@@ -158,12 +179,7 @@ const HomePageProfile = () => {
               College
             </option>
             {allClgs.length > 0 &&
-              allClgs.map((clg) => (
-                <option value={clg}>
-                  {clg}
-                </option>
-              ))
-            }
+              allClgs.map((clg) => <option value={clg}>{clg}</option>)}
           </select>
         </div>
 
@@ -205,13 +221,9 @@ const HomePageProfile = () => {
               <h className=" text-[18px] md:text-[16px]   font-semibold">
                 Total Events:
               </h>
-              <p className=" text-[23px] font-bold p-0 relative bottom-2">
-                10
-              </p>
+              <p className=" text-[23px] font-bold p-0 relative bottom-2">10</p>
             </div>
           </div>
-
-
         </div>
       </div>
     </div>
--- frontend/src/component/approvals/PendingApprovals.jsx
+++ frontend/src/component/approvals/PendingApprovals.jsx
@@ -1,40 +1,46 @@
-import { faCircleCheck, faCircleXmark, faFileInvoice, faSearch,} from "@fortawesome/free-solid-svg-icons";
+import {
+  faCircleCheck,
+  faCircleXmark,
+  faFileInvoice,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
 import "./PendingApprovals.css";
 import { Scrollbars } from "react-custom-scrollbars";
 
 const PendingApprovals = (props) => {
-  const [data,setData] = useState([]);
+  const [data, setData] = useState([]);
   const [searchval, setSearchVal] = useState("");
   const [pendingUsers, setPendingUsers] = useState([]);
-  const [loading,setLoading] = useState(false);
-  const [val,setVal]=useState(false);
+  const [loading, setLoading] = useState(false);
+  const [val, setVal] = useState(false);
 
   const getUser = async () => {
     const result = await fetch(`http://localhost:8000/get`);
     const res = await result.json();
-    let user=[];
-    res && res.map((data)=>{
-      if(data.role=='user'){
-        user.push(data)
-      }
-    })
+    let user = [];
+    res &&
+      res.map((data) => {
+        if (data.role == "user") {
+          user.push(data);
+        }
+      });
     setPendingUsers(user);
     setData(user);
   };
 
   useEffect(() => {
     getUser();
-    setLoading(false);
-  }, [loading])
+    // setLoading(false);
+  });
 
   // search for a pending user
   const searchHandler = (e) => {
     let val = e.target.value;
     setSearchVal(e.target.value);
     let matched = [];
-    if (e.target.value != '') {
+    if (e.target.value != "") {
       data.length > 0 &&
         data.forEach((user) => {
           const value = user.name.toLowerCase().includes(val.toLowerCase());
@@ -44,33 +50,35 @@ const PendingApprovals = (props) => {
         });
       setPendingUsers(matched);
     } else {
-      setPendingUsers(data.length>0 && data);
+      setPendingUsers(data.length > 0 && data);
     }
   };
 
   // Decline request for club member
   const handleDecline = async (id) => {
+    setLoading(true);
     console.log(id);
     const data = await fetch(`http://localhost:8000/user/${id}`, {
-      method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' }
-    })
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    });
     const res = await data.json();
-    setLoading(true)
-  }
+    setLoading(false);
+  };
 
   // Accept request for club member
   const handleAccept = async (id) => {
+    setLoading(true);
     const data = await fetch(`http://localhost:8000/updateDetail/${id}`, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ role: 'Club_Member' })
-    })
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ role: "Club_Member" }),
+    });
     const res = await data.json();
-    setLoading(true)
-    setVal(!val)
+    setVal(!val);
     props.func(!val);
-  }
+    setLoading(false);
+  };
 
   return (
     <div className="PendingApprovals ">
@@ -96,37 +104,59 @@ const PendingApprovals = (props) => {
         <Scrollbars style={{ height: "230px" }}>
           <table class="table-auto w-full max-w-[1300px] ">
             <tbody class="text-sm divide-y divide-gray-100 max-w-[1150px]">
-              {pendingUsers.length > 0 ? pendingUsers.map((approval) => (
-                <tr className="flex justify-between max-w-[1150px]">
-                  <td class="p-2  lg:w-[300px]">
-                    <div className="flex items-center">
-                      <img
-                        class="rounded-full"
-                        src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-05.jpg"
-                        width="40"
-                        height="40"
-                        alt="Alex Shatov"
-                      />
-                      <div className="ml-2"> {approval.name} </div>
-                    </div>
-                  </td>
-                  <td class="p-2 lg:flex items-center hidden md:block">
-                    <div class="font-medium text-gray-800">{approval.collegeYear} year-{approval.branch}</div>
-                  </td>
-                  <td class="pt-2 pb-2 flex justify-end">
-                    <div className="flex items-center font-medium lg:gap-3 justify-start mr-6 md:mr-6 lg:mr-6 2xl:-mr-4  w-fit">
-                      <button className="h-[30px] rounded-xl text-[#616161] font-bold hover:bg-gray-300 mr-2" onClick={() => handleDecline(approval._id)}>
-                        Decline
-                      </button>
-                      <button className="h-[25px] w-[60px] rounded-xl text-white bg-[#00D22E] hover:bg-[#03821f]" onClick={() => handleAccept(approval._id)}>
-                        Accept
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              )) : <div>
-                No Pending Requests...
-              </div> }
+              {pendingUsers.length > 0 ? (
+                pendingUsers.map((approval) => (
+                  <tr className="flex justify-between max-w-[1150px]">
+                    <td class="p-2  lg:w-[300px]">
+                      <div className="flex items-center">
+                        <img
+                          class="rounded-full"
+                          src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-05.jpg"
+                          width="40"
+                          height="40"
+                          alt="Alex Shatov"
+                        />
+                        <div className="ml-2"> {approval.name} </div>
+                      </div>
+                    </td>
+                    <td class="p-2 lg:flex items-center hidden md:block">
+                      <div class="font-medium text-gray-800">
+                        {approval.collegeYear} year-{approval.branch}
+                      </div>
+                    </td>
+                    <td class="pt-2 pb-2 flex justify-end">
+                      <div className="flex items-center font-medium lg:gap-3 justify-start mr-6 md:mr-6 lg:mr-6 2xl:-mr-4  w-fit">
+                        <button
+                          className="h-[30px] rounded-xl text-[#616161] font-bold hover:bg-gray-300 mr-2"
+                          onClick={() => handleDecline(approval._id)}
+                        >
+                          Decline
+                        </button>
+                        {loading  ? (
+                          <div
+                            class="spinner-border text-success"
+                            role="status"
+                            style={{ height: "15px", width: "15px" }}
+                          >
+                            <span class="visually-hidden">Loading...</span>
+                          </div>
+                        ) : 
+                        <button
+                            className="h-[25px] w-[60px] rounded-xl text-white bg-[#00D22E] hover:bg-[#03821f]"
+                            onClick={() => handleAccept(approval._id)}
+                          >
+                            Accept
+                          </button>
+                        }
+
+                        
+                      </div>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <div>No Pending Requests...</div>
+              )}
             </tbody>
           </table>
         </Scrollbars>
