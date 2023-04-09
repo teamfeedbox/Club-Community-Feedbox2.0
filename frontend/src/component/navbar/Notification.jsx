140f9efb4a646aae185cc639592bcb7de6dc9a01
umesh bhatiya <umesh.bhatiya515@gmail.com>
2023-04-09 21:49:46 +0530

    changes done while testing

--- frontend/src/component/navbar/Notification.jsx
+++ frontend/src/component/navbar/Notification.jsx
@@ -64,17 +64,17 @@ const Notification = (props) => {
                 </div>
               </div>)
               :
-              ((data.messageScope === "private" && currentUserId === data.userId) ?
+              ((data.messageScope === "private" && data.message !== "Congrats! +10 coins added." && currentUserId === data.userId) ?
                 <div key={data.message} className="flex bg-blue-200 mt-2 rounded-sm">
-                  <div className="bg-blue-800 p-1 w-[5px]  text-blue-800"></div>
+                  <div className="bg-green-800 p-1 w-[5px]  text-blue-800"></div>
                   <div className="p-1">
                     {data.message}
                   </div>
                 </div>
                 :
-                ((data.message === "Congrats! +10 coins added." && currentUserId === data.userId) &&
+                ((data.messageScope==="private" && data.message === "Congrats! +10 coins added." && currentUserId === data.userId) &&
                   <div key={data.message} className="flex bg-blue-200 mt-2 rounded-sm">
-                    <div className="bg-blue-800 p-1 w-[5px]  text-blue-800"></div>
+                    <div className="bg-yellow-800 p-1 w-[5px]  text-blue-800"></div>
                     <div className="p-1">
                       {data.message}
                     </div>
