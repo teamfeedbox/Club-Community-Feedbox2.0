36efb7cef56cc7ac917ac6cb3bb094b0f3a1607d
GYANESH <gyaneshr7@gmail.com>
2024-03-26 11:11:16 +0530

    Added store component

---
+++ frontend/src/component/store/Store.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar  from './Sidebar';
+
+const Store = () => {
+  return (
+    <>
+    <div className=''>Store</div>
+    </>
+  )
+}
+
+export default Store;
