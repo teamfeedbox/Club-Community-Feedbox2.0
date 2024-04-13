2bfa81d93f762d4679e6a3b7796a11e82ebaf1e3
kartikeyamathur07 <135942680+kartikeyamathur07@users.noreply.github.com>
2024-04-13 09:59:57 +0530

    Update ReactBigCalendar.js

--- frontend/src/component/Calendar/ReactBigCalendar.js
+++ frontend/src/component/Calendar/ReactBigCalendar.js
@@ -442,7 +442,7 @@ export default function ReactBigCalendar() {
           {role && (role == "Super_Admin" || role === "Admin") ? (
             <div className=" my-4 mx-1 ">
               <select
-                className="p-2 border-2 font-semibold text-[#3174AD] border-[#3174AD] rounded-3xl w-[100%]"
+                className="p-2 border-2 font-semibold text-[#3174AD] border-[#3174AD] rounded-3xl w-[100%] cursor-pointer"
                 value={clgSelected}
                 onChange={(e) => {
                   handleCollege(e);
