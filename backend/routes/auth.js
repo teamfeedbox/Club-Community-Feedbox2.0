a2e5d305edcd020ac30e2e950bcd62c82199c67b
teamfeedbox <120553699+teamfeedbox@users.noreply.github.com>
2024-06-12 17:07:56 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -329,7 +329,7 @@ var mailOptions = {
   html:`<div>Dear ${result.name},
        <br/><br/>
        We are pleased to inform you that your account has been successfully verified. You can now login to your account and start using all the features and services that our platform has to offer.<br/>
-       To access your account, please visit our website at [website URL] and click on the login button. If you have any questions or concerns, please do not hesitate to contact us. Our support team is available 24/7 to assist you with any issues you may have.
+       To access your account, please visit our website at https://feedbox.club/ and click on the login button. If you have any questions or concerns, please do not hesitate to contact us. Our support team is available 24/7 to assist you with any issues you may have.
        <br/><br/>
        Best regards,
        <br/><br/>
